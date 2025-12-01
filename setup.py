0e9eabf30c445239f4ede15250475a800db27bdb
subham-rakshit <subhamrakshit667@gmail.com>
2025-12-02 01:13:11 +0530

    requirements changes

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="medical-chatbot",
+    version="0.0.1",
+    author="Subham Rakshit",
+    author_email="subhamrakshit667@gmail.com",
+    description="Medical Chatbot",
+    packages=find_packages(),
+    install_requires=[]
+)
