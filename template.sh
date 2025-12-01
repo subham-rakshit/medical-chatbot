a07430b3fcc92b24a0445a137d554ec05f4ad81d
subham-rakshit <subhamrakshit667@gmail.com>
2025-12-02 00:34:25 +0530

    Folder structure added

---
+++ template.sh
@@ -0,0 +1,18 @@
+# Create directories
+mkdir -p src
+mkdir -p research
+
+# Creating Files
+touch src/__init__.py
+touch src/helper.py
+touch src/prompt.py
+touch .env
+touch setup.py
+touch app.py
+touch research/trials.ipynb
+touch requirements.txt
+
+# After creating files successfully
+echo "Directory and files created successfully!"
+
+# Got to Git Bash the run $ sh template.sh (It will create the directory and files)
